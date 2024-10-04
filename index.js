dc6d7bbaeac2f5af29c88e2a358c0f5b94da6166
Ethan Warden <ethancwarden@gmail.com>
2024-10-04 15:45:48 -0400

    Version 0.1

---
+++ index.js
@@ -0,0 +1,20 @@
+const inputBtn = document.getElementById("input-btn");
+let myLeads = ["lead 1", "lead 2"];
+const inputEl = document.getElementById("input-el")
+const ulEl = document.getElementById("ul-el")
+
+inputBtn.addEventListener("click", function(){
+    myLeads.push(inputEl.value)
+    console.log(myLeads)
+    inputEl.value = ""
+    renderLeads()
+})
+
+function renderLeads() {
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+    listItems += "<li><a href='" + myLeads[i] + "' target='_blank'>" + myLeads[i] + "</a></li>"
+    }
+
+    ulEl.innerHTML = listItems
+}
